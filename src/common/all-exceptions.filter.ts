cbc7d785081737ad188a21e5402380e6ed73c8b9
yeshwanthchillara <ch.yeshwanth.naidu@gmail.com>
2026-06-12 01:37:48 +0530

    fixed global validation error

--- src/common/all-exceptions.filter.ts
+++ src/common/all-exceptions.filter.ts
@@ -10,9 +10,12 @@ import { Request, Response } from 'express';
 
 @Catch()
 export class AllExceptionsFilter implements ExceptionFilter {
-  catch(exception: unknown, host: ArgumentsHost) {
-    const ctx = host.switchToHttp();
+  catch(
+    exception: unknown,
 
+    host: ArgumentsHost,
+  ) {
+    const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
 
@@ -20,16 +23,18 @@ export class AllExceptionsFilter implements ExceptionFilter {
       exception instanceof HttpException
         ? exception.getStatus()
         : HttpStatus.INTERNAL_SERVER_ERROR;
+    let errorResponse: any = {
+      success: false,
+      message: 'Internal server error',
+    };
 
-    const message =
-      exception instanceof HttpException
-        ? exception.message
-        : 'Internal server error';
+    if (exception instanceof HttpException) {
+      errorResponse = exception.getResponse();
+    }
 
     response.status(status).json({
-      success: false,
+      ...errorResponse,
       statusCode: status,
-      message,
       timestamp: new Date().toISOString(),
       path: request.url,
     });
